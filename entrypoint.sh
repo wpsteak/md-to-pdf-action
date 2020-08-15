c06c2a83b1ecd70c6aecd38020270a0d486f6438
Pin Shih Wang <wpsteak@users.noreply.github.com>
2020-08-15 08:08:12 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 # apt-get install libnss3-dev libgconf-2-4 -y
 # apt-get install -y libnss3-tools
-cat *.md > all.md
+
+# cat *.md > all.md
 npm i -g --unsafe-perm md-to-pdf
 
-cat all.md | md-to-pdf --stylesheet s.css > output.pdf
+md-to-pdf
+# cat all.md | md-to-pdf --stylesheet s.css > output.pdf
 
-rm all.md
+# rm all.md
